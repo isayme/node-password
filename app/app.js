f55bf8ec330165eb923000dc42a20c5fcd609596
isayme <isaymeorg@gmail.com>
2018-08-09 17:10:10 +0800

    add hash/verify api

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 const config = require('config')
 const pkg = require('../package.json')
+const passwordUtil = require('./password')
 
 const app = new Koa()
+app.use(bodyParser())
 
 const router = new Router()
 
@@ -12,6 +15,20 @@ router.get('/version', (ctx, next) => {
     name: pkg.name,
     version: pkg.version
   }
+})
+
+router.post('/hash', async (ctx, next) => {
+  const { algorithm, password, options } = ctx.request.body
+  const hashed = await passwordUtil.hash(algorithm, password, options)
+
+  ctx.body = { hashed }
+})
+
+router.post('/verify', async (ctx, next) => {
+  const { hashed, password } = ctx.request.body
+  const match = await passwordUtil.verify(hashed, password)
+
+  ctx.body = { match }
 })
 
 app.use(router.routes()).use(router.allowedMethods())
---
+++ app/password.js
@@ -0,0 +1,17 @@
+const upash = require('upash')
+
+upash.install('pbkdf2', require('@phc/pbkdf2'))
+upash.install('argon2', require('@phc/argon2'))
+upash.install('scrypt', require('@phc/scrypt'))
+upash.install('bcrypt', require('@phc/bcrypt'))
+
+module.exports = {
+  // default use pbkdf2
+  hash (algorithm = 'pbkdf2', password, options) {
+    return upash.use(algorithm).hash(password, options)
+  },
+
+  verify (hashstr, password) {
+    return upash.verify(hashstr, password)
+  }
+}
